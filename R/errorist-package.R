a98002417b9e7b12a72f3609f5163f19d14fd262
James Balamuta <balamut2@illinois.edu>
2020-01-03 23:45:38 -0600

    Change how package-level documentation is generated

--- R/errorist-package.R
+++ R/errorist-package.R
@@ -1,3 +1,8 @@
-#' @details
-#' By default, `errorist` searches Google for both warnings and errors.
+#' @keywords internal
 "_PACKAGE"
+
+# The following block is used by usethis to automatically manage
+# roxygen namespace tags. Modify with care!
+## usethis namespace: start
+## usethis namespace: end
+NULL
